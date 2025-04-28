bdb74e295881a7d689b31893e0ce81c48065b425
Adedoyin13 <olajideadedoyinrodiyat@gmail.com>
2025-04-28 16:07:17 +0100

    update

--- server/server.js
+++ server/server.js
@@ -72,7 +72,7 @@ app.use(
 // CORS setup
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
     credentials: true,
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS",
   })
